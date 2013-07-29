0a53b44a106465afb1fae23f45625f8103bb5b54
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2013-07-29 00:42:26 -0400

    Fixed inheritance

--- index.js
+++ index.js
@@ -3,23 +3,22 @@ var util = require('util'),
 
 function EmitterScopeRelay() {
 	events.EventEmitter.call(this);
-
-	this.relay = function (event, broadcast) {
-		var self = this;
-		this.on(event, function () {
-			var f = broadcast ? self.scope.$broadcast : self.scope.$emit,
-				args = Array.prototype.slice.call(arguments);
-
-			f.apply(self.scope, [event].concat(args));
-		});
-		return this;
-	};
-
-	this.to = function (scope) {
-		this.scope = scope;
-	};
 }
-
 util.inherits(EmitterScopeRelay, events.EventEmitter);
+
+EmitterScopeRelay.prototype.relay = function (event, broadcast) {
+	var self = this;
+	this.on(event, function () {
+		var f = broadcast ? self.scope.$broadcast : self.scope.$emit,
+			args = Array.prototype.slice.call(arguments);
+
+		f.apply(self.scope, [event].concat(args));
+	});
+	return this;
+};
+
+EmitterScopeRelay.prototype.to = function (scope) {
+	this.scope = scope;
+};
 
 module.exports = EmitterScopeRelay;
